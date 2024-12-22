ed4cc49b008e516d89cbdf6805e78d06397026cc
aartoni <alessio@artoni.org>
2024-12-22 11:31:38 +0700

    feat: get nodes and edges

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,38 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let parser = CertParser::from_file(path).context("Failed to create reader")?;
 
     let certs: Vec<_> = parser.flatten().collect();
+    let nodes: Vec<_> = certs
+        .iter()
+        .map(|c| c.fingerprint().to_spaced_hex())
+        .collect();
+
+    let signatures = certs.iter().flat_map(|c| {
+        c.primary_key()
+            .certifications()
+            .map(|s| (c.fingerprint(), s))
+    });
+
+    let mut edges = Vec::new();
+
+    for (fingerprint, signature) in signatures {
+        println!("Signature found!");
+        let signee = fingerprint.to_spaced_hex();
+        signature
+            .get_issuers()
+            .iter()
+            .map(|i| match i {
+                KeyHandle::Fingerprint(fp) => fp.to_spaced_hex(),
+                KeyHandle::KeyID(id) => {
+                    eprintln!("Signed using a KeyID! {id}");
+                    id.to_spaced_hex()
+                }
+            })
+            .for_each(|i| edges.push((signee.clone(), i)));
+    }
 
     // TODO Write to DB
+    drop(nodes);
+    drop(edges);
 
     // TODO Perform a simple query
 
