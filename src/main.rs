90c9b5f6c3592e7990a3b37364fd1f79d389406d
aartoni <alessio@artoni.org>
2024-12-26 21:30:32 +0700

    fix: avoid constraint violation

--- src/main.rs
+++ src/main.rs
@@ -32,18 +32,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     tracing::info!("Finished parsing certificates.");
-    let edges = graph.edges();
 
     // Write to DB
-
-    // Add nodes
-    let mut txn = db.start_txn().await.unwrap();
-    let queries = graph
+    let mut nodes_txn = db.start_txn().await.unwrap();
+    let node_queries = graph
         .nodes()
         .iter()
-        .map(|fp| query("CREATE (k:Key {fingerprint: $fp})").param("fp", fp.as_str()));
-    txn.run_queries(queries).await.unwrap();
-    txn.commit().await.unwrap();
+        .map(|fp| query("MERGE (k:Key {fingerprint: $fp})").param("fp", fp.as_str()));
+    nodes_txn.run_queries(node_queries).await.unwrap();
+    nodes_txn.commit().await.unwrap();
+
+    // Edges txn
+    let mut edges_txn = db.start_txn().await.unwrap();
+    let edge_queries = graph.edges().into_iter().map(|(signer, signee)| {
+        query("MERGE (signer:Key {fingerprint: $signer}) MERGE (signee:Key {fingerprint: $signer}) MERGE (signer) -[:SIGNED]-> (signee)")
+            .param("signer", signer.as_str())
+            .param("signee", signee.as_str())
+    });
+    edges_txn.run_queries(edge_queries).await.unwrap();
+    edges_txn.commit().await.unwrap();
 
     // TODO Perform a simple query
 
