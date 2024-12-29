6bda5ff66d2fa847950278ef4f68ee8a6a003e94
aartoni <alessio@artoni.org>
2024-12-29 21:06:12 +0700

    feat: batch requests to lower mem requirements

--- loader/src/main.rs
+++ loader/src/main.rs
@@ -1,5 +1,6 @@
 #![feature(hash_set_entry)]
 use dotenvy::dotenv;
+use itertools::Itertools;
 use libsdop::db::DatabaseBuilder;
 use loader::{get_cert_paths, get_certs, setup_database, sync_cache, Graph};
 use neo4rs::query;
@@ -34,23 +35,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing::info!("Finished parsing certificates.");
 
     // Write to DB
-    let mut nodes_txn = db.start_txn().await?;
     let node_queries = graph
         .nodes()
         .iter()
-        .map(|fp| query("MERGE (k:Key {fingerprint: $fp})").param("fp", fp.as_str()));
-    nodes_txn.run_queries(node_queries).await?;
-    nodes_txn.commit().await?;
+        .map(|fp| query("MERGE (k:Key {fingerprint: $fp})").param("fp", fp.as_str()))
+        .chunks(100);
+
+    for query in &node_queries {
+        let mut nodes_txn = db.start_txn().await?;
+        nodes_txn.run_queries(query).await?;
+        nodes_txn.commit().await?;
+    }
 
     // Edges txn
-    let mut edges_txn = db.start_txn().await?;
     let edge_queries = graph.edges().into_iter().map(|(signer, signee)| {
         query("MERGE (signer:Key {fingerprint: $signer}) MERGE (signee:Key {fingerprint: $signee}) MERGE (signer) -[:SIGNED]-> (signee)")
-            .param("signer", signer.as_str())
-            .param("signee", signee.as_str())
-    });
-    edges_txn.run_queries(edge_queries).await?;
-    edges_txn.commit().await?;
+        .param("signer", signer.as_str())
+        .param("signee", signee.as_str())
+    }).chunks(100);
+
+    for query in &edge_queries {
+        let mut edges_txn = db.start_txn().await?;
+        edges_txn.run_queries(query).await?;
+        edges_txn.commit().await?;
+    }
+
     tracing::info!("Done loading nodes and edges");
 
     Ok(())
