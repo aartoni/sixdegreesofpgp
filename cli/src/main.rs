e5dd69375b873dec96c6adf3f2c285ce62f246ae
aartoni <alessio@artoni.org>
2024-12-29 12:36:09 +0700

    feat: remove tracing from cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,16 +2,11 @@ use clap::{Arg, Command};
 use dotenvy::dotenv;
 use libsdop::db::DatabaseBuilder;
 use neo4rs::{query, Path};
-use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Setup
     dotenv()?;
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .with_env_filter(EnvFilter::from_default_env())
-        .init();
     let db = DatabaseBuilder::from_env()?.build().await?;
 
     // Parse args
