3f06074071b9827e08e62ee9f22c07dd3f581cab
aartoni <alessio@artoni.org>
2025-01-16 21:06:56 +0100

    feat: rsync dump update

--- loader/src/lib.rs
+++ loader/src/lib.rs
@@ -1,7 +1,7 @@
 #![feature(hash_set_entry)]
 use neo4rs::{query, Result};
 use sequoia_openpgp::{cert::CertParser, Cert};
-use std::{path::PathBuf, rc::Rc};
+use std::{path::PathBuf, process::Command, rc::Rc};
 
 mod graph;
 mod sig_store;
@@ -25,7 +25,19 @@ pub async fn setup_database(db: &neo4rs::Graph) -> Result<()> {
 }
 
 pub fn sync_cache() {
-    tracing::warn!("The sync feature hasn't been implemented yet");
+    let path = dirs::cache_dir()
+        .and_then(|path| path.to_str().map(|s| s.to_owned()))
+        .expect("No cache dir found");
+    let status = Command::new("rsync")
+        .args(&["-av", "rsync://rsync.cyberbits.eu/sks/dump/", &path])
+        .status()
+        .expect("Failed to execute rsync");
+
+    if status.success() {
+        tracing::info!("Hockeypuck dump updated");
+    } else {
+        tracing::error!("Couldn't update the Hockeypuck dump");
+    }
 }
 
 pub fn get_cert_paths() -> impl Iterator<Item = PathBuf> {
